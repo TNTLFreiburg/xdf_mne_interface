b6569224ad8f8de5f38ddab54326b3ad5d702eb3
HenrikBons <henrikbons@gmail.com>
2019-09-09 12:39:21 +0200

    Update mne_interface.py
    included online pipline as 'bdonline_extract()' function

--- mne_interface.py
+++ mne_interface.py
@@ -7,6 +7,7 @@ import mne
 import cv2
 from braindecode.datautil.signalproc import exponential_running_standardize, exponential_running_demean
 from scipy.signal import filtfilt, iirnotch, butter
+from processors import StandardizeProcessor
 
 
 def xdf_loader(xdf_file):
@@ -368,6 +369,116 @@ def dlvr_braindecode(path, files, timeframe_start, target_fps):
         y = np.append(y,key)
     y = y.astype(np.int64)
     return(X,y)
+
+
+def bdonline_extract(path, files, timeframe_start, target_fps):
+    """ Uses event markers to extract motor tasks from multiple DLVR .xdf files.
+    Args:
+        path: If the files share a single path, you can specify it here.
+        
+        files: A list of .xdf files to extract data from.
+        
+        timeframe_start: The time in seconds before the event, in which the EEG Data is extracted.
+        
+        target_fps: Downsample the EEG-data to this value.         
+    
+    Returns:
+        X: A list of trials
+        y: An array specifying the action
+    """
+    
+    DOWNSAMPLING_COEF = int(5000 / target_fps)
+    processor = StandardizeProcessor()
+    # Epochs list containing differently sized arrays [#eeg_electrodes, times]
+    X = []
+    #event ids corresponding to the trials where 'left' = 0 and 'right' = 1
+    y = np.array([])
+    for file in files:
+        #load a file
+        print('Reading ', file)
+        current_raw = xdf_loader(path+file)
+        
+        # For MEGVR experiments switch EMG into C3/4
+        current_raw._data[14,:]= current_raw.get_data(picks = ['EMG_LH']) #C3
+        current_raw._data[16,:]= current_raw.get_data(picks = ['EMG_RH']) #C4
+        
+        #discard EOG/EMG
+        current_raw.pick_types(meg=False, eeg=True)
+        #pick only relevant events
+        events = current_raw.events[(current_raw.events[:,2]==current_raw.event_id['Monster left']) | (current_raw.events[:,2]==current_raw.event_id['Monster right'])]
+        #timestamps where a monster deactivates
+        stops = current_raw.events[:,0][(current_raw.events[:,2]==current_raw.event_id['Monster destroyed'])]
+        #timestamps where trials begin
+        starts = events[:,0]
+    
+        #extract event_ids and shift them to [0, 1]
+        key = events[:,2]
+        key = (key==key.max()).astype(np.int64)
+        
+        #standardize, convert to size(time, channels)
+        #current_raw._data = exponential_running_standardize(current_raw._data.T, factor_new=0.001, init_block_size=None, eps=0.0001).T
+        
+        #Find the trials and their corresponding end points
+        #filter signal
+        B_1, A_1 = butter(5, 1, btype='high', output='ba', fs = 5000)
+
+        # Butter filter (lowpass) for 30 Hz
+        B_40, A_40 = butter(6, 120, btype='low', output='ba', fs = 5000)
+
+        # Notch filter with 50 HZ
+        F0 = 50.0
+        Q = 30.0  # Quality factor
+        # Design notch filter
+        B_50, A_50 = iirnotch(F0, Q, 5000)
+        
+        bads = np.array([])
+        for count, event in enumerate(starts):
+            #in case the last trial has no end (experiment ended before the trial ends), discard it
+            if len(stops[stops>event]) == 0:
+                key = key[:-sum(starts>=event)]
+                break
+                
+            if stops[stops>event][0]-event < 5000:
+                bads = np.append(bads,count)                
+                continue
+            
+            #Get the trial from 1 second before the task starts to the next 'Monster deactived' flag
+            current_epoch = np.array([]) #current_raw._data[:, event-round(timeframe_start*5000) : stops[stops>event][0]]
+            #400 samples each
+            ii = event-round(timeframe_start*5000)
+            while ii < stops[stops>event][0]:
+                current_chunk = current_raw._data[:,ii:ii+400] 
+                ii += 400
+        
+                current_chunk = filtfilt(B_50, A_50, current_chunk)
+                current_chunk = filtfilt(B_40, A_40, current_chunk)            
+            
+                #downsample to 250 Hz
+                current_chunk = np.array([np.mean(current_chunk[:, i:i + DOWNSAMPLING_COEF], axis=1) for i in
+							np.arange(0, 400, DOWNSAMPLING_COEF)]).astype('float32')
+                
+            
+                #standardize, convert to size(time, channels)
+                #current_epoch = exponential_running_standardize(current_epoch.T, factor_new=0.001, init_block_size=None, eps=0.0001).T
+                current_chunk = processor.process(current_chunk).T
+                
+                current_chunk = current_chunk.astype(np.float32)
+                
+                if current_epoch.size == 0:
+                    current_epoch = current_chunk                  
+                else:
+                    current_epoch = np.append(current_epoch, current_chunk, 1)  
+                    
+            
+            X.append(current_epoch)
+        
+        if len(bads)>0:
+            key = np.delete(key, bads)
+        y = np.append(y,key)
+    y = y.astype(np.int64)
+    return(X,y)
+
+
 
 def plot_relative_psd(epochs, events, fmax=np.inf):
     epochs.pick_types(meg=False, eeg=True)
